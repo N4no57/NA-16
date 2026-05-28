c71e61e02b799a9ba4190d0a076768b7506843c9
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 10:37:50 +0100

    symbol table

--- assembler/src/codegen/codegen.h
+++ assembler/src/codegen/codegen.h
@@ -7,6 +7,16 @@
 #define GEN_MEX(DEST_MODE, SRC1_MODE, SRC2_MODE) ((0x8 << 12) | (((DEST_MODE) & 0xF) << 8) | (((SRC1_MODE) & 0xF) << 4) | ((SRC2_MODE) & 0xF))
 #define GEN_AEX (0x91)
 
+typedef struct {
+    NodeSymbol *symbols;
+    u64 count;
+    u64 size;
+} SymbolTable;
+
+void init_symbol_table(SymbolTable *table);
+void push_symbol(SymbolTable *table, NodeSymbol *symbol);
+
+void symbol_pass(NodeProgram *ast, SymbolTable *symtbl);
 void generate_code(NodeProgram *ast, bytes *code);
 
 #endif //NA_16_CODEGEN_H
---
+++ assembler/src/codegen/symbol_pass.c
@@ -0,0 +1,9 @@
+#include "codegen.h"
+
+void init_symbol_table(SymbolTable *table) {
+
+}
+
+void push_symbol(SymbolTable *table, NodeSymbol *symbol) {
+
+}
