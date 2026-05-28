8c479a85aff465f16f4f8984b2882d8f78572a9a
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 20:37:00 +0100

    fixed pop

--- emulator/src/cpu/instructions/handlers/data_movement/pop.c
+++ emulator/src/cpu/instructions/handlers/data_movement/pop.c
@@ -5,11 +5,10 @@ void pop_handler(CPU *cpu, Instruction *inst) {
     u16 source = 0;
 
     if (inst->ops[0].size == 2) {
-        source = read_byte(cpu, cpu->SP++) << 8;
-        source |= read_byte(cpu, cpu->SP++);
-        read_byte(cpu, cpu->SP++);
+        source = read_byte(cpu, ++cpu->SP) << 8;
+        source |= read_byte(cpu, ++cpu->SP);
     } else if (inst->ops[0].size == 1) {
-        source = read_byte(cpu, cpu->SP++);
+        source = read_byte(cpu, ++cpu->SP);
     }
 
     operand_write(cpu, inst->ops[0], source);
