6fe3d5281ecc7394ef47c0bc8421dd98aa198bb8
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 11:52:59 +0100

    increase version counter

--- assembler/src/main.c
+++ assembler/src/main.c
@@ -64,7 +64,7 @@
 i32 error_count = 0;
 
 #define OUTPUT_DEFAULT "a.out"
-#define ASSEMBLER_VERSION "1.3.0"
+#define ASSEMBLER_VERSION "1.4.0"
 
 const char *ChangeFileExt(char *string) {
     for (int i = 0; i < strlen(string); i++) {
