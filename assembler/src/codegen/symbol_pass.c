414db1c41b15c0db948a18c182121a864fa9a6d7
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 11:12:00 +0100

    shtuff

--- assembler/src/codegen/symbol_pass.c
+++ assembler/src/codegen/symbol_pass.c
@@ -23,22 +23,46 @@ NodeSymbol *find_symbol(SymbolTable *table, char *symbol) {
             return &table->symbols[i];
         }
     }
+
     return nullptr;
 }
 
-void visit_NodeOperand(const NodeOperand *operand, bool use_16bits);
+void visit_NodeOperand1(const NodeOperand *operand, bool use_16bits);
 
-void visit_NodeInstruction(const NodeInstruction *node, SymbolTable *table);
+void visit_NodeInstruction1(const NodeInstruction *node, SymbolTable *table, u64 *byte_pos) {
+    InstructionSpec info = get_spec(node->mnemonic);
 
-void visit_NodeSymbol(const NodeSymbol *node, SymbolTable *table) {
+    u64 sig_id = 0;
+    InstructionSignature *sig = &info.signatures[sig_id];
+    for (sig_id = 0; sig_id < info.signature_count; sig_id++) {
+        if (match_signature(node, sig)) break;
+        sig = &info.signatures[sig_id+1];
+    }
 
+    if (sig_id > 0) {
+        *byte_pos += 2;
+    }
 }
 
-void visit_NodeStatement(const NodeStatement *node, SymbolTable *table) {
+void visit_NodeSymbol1(NodeSymbol *node, SymbolTable *table, const u64 *byte_pos) {
+    NodeSymbol *symbol = find_symbol(table, node->symbol_name);
+
+    if (symbol) {
+        error(node->pos, "Reused symbol");
+    }
+
+    if (node->value == -1) { // it's a label
+        node->value = *(i32 *)byte_pos;
+    }
+
+    push_symbol(table, *node);
+}
+
+void visit_NodeStatement1(NodeStatement *node, SymbolTable *table, u64 *byte_pos) {
     if (node->kind == ST_INSTRUCTION) {
-        visit_NodeInstruction(&node->instruction, table);
+        visit_NodeInstruction1(&node->instruction, table, byte_pos);
     } else if (node->kind == ST_SYMBOL) {
-        visit_NodeSymbol(&node->symbol, table);
+        visit_NodeSymbol1(&node->symbol, table, byte_pos);
     }
 }
 
