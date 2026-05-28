51db54cf8ef7a0b49060e025e14332ff713aec1f
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 10:26:51 +0100

    bleh

---
+++ assembler/src/semantic analyser/analyser.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+#include "../lib/error.h"
+#include "analyser.h"
+
+i32 validate_instruction(const NodeInstruction* inst) {
+    InstructionSpec spec = get_spec((char *)inst->mnemonic);
+
+    if (spec.mnemonic == nullptr) return 0;
+
+    if (match_signature(inst, &spec.signature)) {
+        return 1;
+    }
+
+    error(inst->pos, "Invalid instruction \"%s\"", inst->mnemonic);
+    return 0;
+}
+
+void analyse(const NodeProgram* ast) {
+    for (u64 i = 0; i < ast->count; i++) {
+        if (ast->statements[i].kind == ST_INSTRUCTION) {
+            validate_instruction(&ast->statements[i].instruction);
+        }
+    }
+}
---
+++ assembler/src/semantic analyser/analyser.h
@@ -0,0 +1,9 @@
+#ifndef NA_16_ANALYSER_H
+#define NA_16_ANALYSER_H
+
+#include "../lib/asmlib.h"
+#include "../parser/ast.h"
+
+void analyse(const NodeProgram* ast);
+
+#endif //NA_16_ANALYSER_H
