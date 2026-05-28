e5551398343c98022f2c63174bb5af0d43a3d6d8
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 20:37:13 +0100

    push relies on operand size only

--- emulator/src/cpu/instructions/handlers/data_movement/push.c
+++ emulator/src/cpu/instructions/handlers/data_movement/push.c
@@ -4,12 +4,6 @@
 void push_handler(CPU *cpu, Instruction *inst) {
     const u16 source = operand_read(cpu, inst->ops[0]);
 
-    if (inst->ops[0].mode == 0) { // register
-        write_byte(cpu, cpu->SP--, source & 0xFF);
-        write_byte(cpu, cpu->SP--, source >> 8);
-        return;
-    }
-
     if (inst->ops[0].size == 2) {
         write_byte(cpu, cpu->SP--, source & 0xFF);
         write_byte(cpu, cpu->SP--, source >> 8);
