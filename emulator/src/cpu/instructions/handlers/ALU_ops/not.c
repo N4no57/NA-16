6c4801ba5cf5653f78c4a4f63a63c66157aea6f5
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 20:05:41 +0100

    fixed misnaming

--- emulator/src/cpu/instructions/handlers/ALU_ops/not.c
+++ emulator/src/cpu/instructions/handlers/ALU_ops/not.c
@@ -1,6 +1,6 @@
 #include "../../inst.h"
 
-void and_handler(CPU *cpu, Instruction *inst) {
+void not_handler(CPU *cpu, Instruction *inst) {
     const u16 l = operand_read(cpu, inst->ops[1]);
 
     operand_write(cpu, inst->ops[0], ~l);
