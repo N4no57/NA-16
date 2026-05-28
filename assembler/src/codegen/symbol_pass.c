12528cf9d563933b9e6a6a3886956a974e97ff0c
NanoDev <b.rego.oliveira@gmail.com>
2026-05-28 10:43:56 +0100

    implemented push_symbol()

--- assembler/src/codegen/symbol_pass.c
+++ assembler/src/codegen/symbol_pass.c
@@ -1,9 +1,15 @@
 #include "codegen.h"
 
-void init_symbol_table(SymbolTable *table) {
+void push_symbol(SymbolTable *table, NodeSymbol symbol) {
+    if (table->count >= table->size) {
+        if (table->size == 0) table->size = 256;
+        else table->size *= 2;
+        NodeSymbol *tmp = realloc(table->symbols, table->size * sizeof(NodeSymbol));
+        if (tmp == nullptr) {
+            exit(EXIT_FAILURE);
+        }
+        table->symbols = tmp;
+    }
 
-}
-
-void push_symbol(SymbolTable *table, NodeSymbol *symbol) {
-
+    table->symbols[table->count++] = symbol;
 }
