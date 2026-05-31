90cbbfeaa5adc39d1e8a46689c7a358f58c1b96b
NanoDev <b.rego.oliveira@gmail.com>
2026-05-31 11:02:55 +0100

    fixed error in ret_handler

--- emulator/src/cpu/instructions/handlers/control_flow/ret.c
+++ emulator/src/cpu/instructions/handlers/control_flow/ret.c
@@ -2,8 +2,8 @@
 #include "../../../memory.h"
 
 void ret_handler(CPU *cpu, Instruction *inst) {
-    u16 address = read_byte(cpu, cpu->SP++) << 8; // pop return address
-    address |= read_byte(cpu, cpu->SP++);
+    u16 address = read_byte(cpu, ++cpu->SP) << 8; // pop return address
+    address |= read_byte(cpu, ++cpu->SP);
 
     cpu->PC = address;
 }
