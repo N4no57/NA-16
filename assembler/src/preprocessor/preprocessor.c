c7371ab7ef276ed3575f8a07b64887a0dcc83b8b
Bernardo <b.rego.oliveira@gmail.com>
2026-06-02 11:48:42 +0100

    refactor

--- assembler/src/preprocessor/preprocessor.c
+++ assembler/src/preprocessor/preprocessor.c
@@ -7,14 +7,14 @@
 #include "../lib/error.h"
 
 // expects idx to be set to the .include directive
-void handle_include(TokenList *tokens, const u64 *idx, Token *tok) {
-    token_delete(tokens, *idx); // remove .include directive
-    if (tokens->tokens[*idx].type != TT_STRING) {
-        error(tokens->tokens[*idx].pos, "Expected a string after \".include\"");
+2void handle_include(TokenList *list, const u64 *idx, Token *tok) {
+    token_delete(list, *idx); // remove .include directive
+    if (list->tokens[*idx].type != TT_STRING) {
+        error(list->tokens[*idx].pos, "Expected a string after \".include\"");
         return;
     }
     char *filename = strdup(tok->value);
-    token_delete(tokens, *idx); // remove argument after .include
+    token_delete(list, *idx); // remove argument after .include
 
     TokenList new_tokens;
     init_TokenList(&new_tokens);
@@ -26,7 +26,7 @@ void handle_include(TokenList *tokens, const u64 *idx, Token *tok) {
     new_tokens.count--; // "pop" EOF token at the end
 
     for (u64 i = 0; i < new_tokens.count; i++) {
-        token_insert(tokens, &new_tokens.tokens[i], i + *idx);
+        token_insert(list, &new_tokens.tokens[i], i + *idx);
     }
 
     free(code);
