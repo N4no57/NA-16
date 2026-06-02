1532c7568ac418a0fd7e3590c70ed802721371c5
NanoDev <b.rego.oliveira@gmail.com>
2026-06-02 16:40:07 +0100

    sections!!!

---
+++ assembler/src/lib/sections.c
@@ -0,0 +1,53 @@
+#include "sections.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+Section defaultSections[] = {
+    {"text", nullptr, 0, 0},
+    {"data", nullptr, 0, 0},
+    {"bss", nullptr, 0, 0},
+    {"rodata", nullptr, 0, 0},
+};
+
+void init_SectionTable(SectionTable *list) {
+    list->count = 4;
+    list->size = 8;
+
+    list->sections = malloc(list->size * sizeof(Section *));
+    if (!list->sections) exit(1);
+
+    memcpy(list->sections, defaultSections, sizeof(defaultSections));
+}
+
+void free_SectionTable(SectionTable *list) {
+    free(list->sections);
+}
+
+void section_push(SectionTable *list, Section *section) {
+    if (list->count >= list->size) {
+        if (list->size == 0) {
+            init_SectionTable(list);
+        } else {
+            list->size *= 2;
+            Section *tmp = realloc(list->sections, list->size * sizeof(Section *));
+            if (!tmp) {
+                exit(1);
+            }
+            list->sections = tmp;
+        }
+    }
+
+    memcpy(&list->sections[list->count], section, sizeof(Section));
+    list->count++;
+}
+
+Section *get_section(SectionTable *list, char *name) {
+    for (u64 i = 0; i < list->count; i++) {
+        if (strcmp(list->sections[i].name, name) == 0) {
+            return &list->sections[i];
+        }
+    }
+
+    return nullptr;
+}
---
+++ assembler/src/lib/sections.h
@@ -0,0 +1,27 @@
+#ifndef NA_16_SECTIONS_H
+#define NA_16_SECTIONS_H
+
+#include "asmlib.h"
+
+typedef struct {
+    char *name;
+
+    // binary data for section
+    u8 *data;
+    u64 count;
+    u64 size;
+} Section;
+
+typedef struct {
+    Section *sections;
+    u64 count;
+    u64 size;
+} SectionTable;
+
+void init_SectionTable(SectionTable *list);
+void free_SectionTable(SectionTable *list);
+
+void section_push(SectionTable *list, Section *section);
+Section *get_section(SectionTable *list, char *name);
+
+#endif //NA_16_SECTIONS_H
