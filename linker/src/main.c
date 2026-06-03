a85b927580fc44add8684102b8c33174162fcf76
NanoDev <b.rego.oliveira@gmail.com>
2026-06-03 15:05:39 +0100

    object read done :)

--- linker/src/main.c
+++ linker/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -33,13 +34,23 @@ typedef struct {
     char *name;
     u64 offset;
     u64 size;
+
+    u8 *data;
+    u64 str_table_ref;
 } Section;
 
 typedef struct {
     char *name;
     u64 section_idx;
     u64 section_offset;
+
+    struct {
+        u8 GLOBAL : 1;
+        u8 DEFINED : 1;
+    };
     u8 status;
+
+    u64 str_table_ref;
 } Symbol;
 
 typedef enum {
@@ -61,12 +72,11 @@ typedef struct {
     Section *section_table;
     Symbol *symbol_table;
     Relocation *relocation_table;
-    char **string_table;
+    char *string_table;
 } ObjectFile;
 
-int main() {
-    ObjectFile obj;
-    ObjectFileHeader *header = &obj.header;
+void read_obj(ObjectFile *obj) {
+    ObjectFileHeader *header = &obj->header;
 
     FILE *f = fopen("test.o", "rb");
     fread(&header->magic, sizeof(header->magic), 1, f);
@@ -80,18 +90,78 @@ int main() {
     fread(&header->string_table_offset, sizeof(header->string_table_offset), 1, f);
     fread(&header->string_table_size, sizeof(header->string_table_size), 1, f);
 
-    long thing = ftell(f);
+    long eoh = ftell(f);
+
     fseek(f, 0, SEEK_END);
-    long thing_2 = ftell(f);
-    long size = thing_2 - thing;
+    u64 string_table_end = ftell(f);
+    obj->string_table = malloc(string_table_end - header->string_table_offset);
+    fseek(f, (long)header->string_table_offset, SEEK_SET);
+    fread(obj->string_table, string_table_end - header->string_table_offset, 1, f);
 
-    void *ptr = malloc(size);
+    fseek(f, eoh, SEEK_SET);
 
-    fseek(f, thing, SEEK_SET);
-    fread(ptr, size, 1, f);
+    // section table
+    obj->section_table = malloc(header->section_table_size * sizeof(Section));
+    for (u64 i = 0; i < header->section_table_size; i++) {
+        Section *section = &obj->section_table[i];
+        fread(&section->str_table_ref, sizeof(section->str_table_ref), 1, f);
+        fread(&section->offset, sizeof(section->offset), 1, f);
+        fread(&section->size, sizeof(section->size), 1, f);
+        section->name = obj->string_table + section->str_table_ref;
+    }
+
+    // symbol table
+    obj->symbol_table = malloc(header->symbol_table_size * sizeof(Symbol));
+    for (u64 i = 0; i < header->symbol_table_size; i++) {
+        Symbol *sym = &obj->symbol_table[i];
+        fread(&sym->str_table_ref, sizeof(sym->str_table_ref), 1, f);
+        fread(&sym->section_idx, sizeof(sym->section_idx), 1, f); // what section am I in?
+        fread(&sym->section_offset, sizeof(sym->section_offset), 1, f); // where am I in the section
+        fread(&sym->status, sizeof(sym->status), 1, f); // info about the symbol e.g. definition or if it is global
+        sym->name = obj->string_table + sym->str_table_ref;
+    }
+
+    // relocation table
+    obj->relocation_table = malloc(header->relocation_table_size * sizeof(Relocation));
+    for (u64 i = 0; i < header->relocation_table_size; i++) {
+        Relocation *reloc = &obj->relocation_table[i];
+        fread(&reloc->symbol_ref, sizeof(&reloc->symbol_ref), 1, f);
+        fread(&reloc->type, sizeof(reloc->type), 1, f);
+        fread(&reloc->section_offset, sizeof(reloc->section_offset), 1, f);
+        reloc->name = obj->symbol_table[reloc->symbol_ref].name;
+    }
+
+    u64 program_offset = ftell(f);
+    u64 program_size = header->string_table_offset - program_offset;
+    void *program = malloc(program_size);
+    fread(program, program_size, 1, f);
 
     fclose(f);
-    free(ptr);
+
+    u64 offset = 0;
+    for (u64 i = 0; i < obj->header.section_table_size; i++) {
+        Section *section = &obj->section_table[i];
+
+        if (section->size == 0) {
+            section->data = nullptr;
+            continue;
+        }
+
+        section->data = program + offset;
+        offset += section->size;
+    }
+}
+
+int main() {
+    ObjectFile obj;
+
+    read_obj(&obj);
+
+    free(obj.section_table[0].data);
+    free(obj.section_table);
+    free(obj.symbol_table);
+    free(obj.relocation_table);
+    free(obj.string_table);
 
     return 0;
 }
