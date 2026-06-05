28fca6c0fec1bcf1bea27e2b0e5ca6dbaa26b92b
Bernardo <b.rego.oliveira@gmail.com>
2026-06-05 12:02:39 +0100

    fix bug by moving functionality into the appropriate header

--- linker/src/lib/global_symbol_table.c
+++ linker/src/lib/global_symbol_table.c
@@ -64,11 +64,11 @@ void glt_push_table(GlobalSymbolTable *table, Symbol *symbols, const u64 count, 
         Symbol *symbol = &table->items[table->count].symbols[i];
         if (symbol->flags & SYM_GLOBAL) {
             Symbol *sym = glt_get_global(table, symbol->name);
-            u64 tmp1 = (u64)sym;
-            u64 tmp2 = (u64)table->global_symbols.symbols;
-            u64 idx = (tmp1 - tmp2) / sizeof(Symbol);
-            char *og_file = table->items[table->global_symbols.file_refs[idx]].filename;
             if (sym) {
+                u64 tmp1 = (u64)sym;
+                u64 tmp2 = (u64)table->global_symbols.symbols;
+                u64 idx = (tmp1 - tmp2) / sizeof(Symbol);
+                char *og_file = table->items[table->global_symbols.file_refs[idx]].filename;
                 printf("\"%s\" is already declared as a global in %s but is redifined as global elsewhere\n", sym->name, og_file);
                 exit(-1);
             }
