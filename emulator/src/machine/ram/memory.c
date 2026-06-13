7872d6c3077fd3a79c3adb8722e4c710b5845ea1
NanoDev <b.rego.oliveira@gmail.com>
2026-06-13 23:55:52 +0100

    this feels a lot nicer

--- emulator/src/machine/ram/memory.c
+++ emulator/src/machine/ram/memory.c
@@ -3,143 +3,63 @@
 #include "../mmu/mmu.h"
 #include "../PIC/pic.h"
 
-bool read_byte(Machine *machine, const u16 address, u64 *value) {
+bool resolve_address(Machine *machine, u64 virtual_address, u64 *physical_address) {
     CPU *cpu = &machine->cpu;
     if (cpu->sys.FR.V  == 1) {
-        u32 true_address = 0;
-        bool success = translate(machine, address, &true_address);
+        bool success = translate(machine, virtual_address, (u32 *)physical_address);
         if (!success) {
-            raise_exception(machine, PF, address);
+            raise_exception(machine, PF, virtual_address);
             return false;
         }
 
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
+        if (*physical_address >= machine->ram.memory_size) {
+            raise_exception(machine, GP, virtual_address);
+            return false;
+        }
+    } else {
+        if (virtual_address >= machine->ram.memory_size) {
+            raise_exception(machine, GP, virtual_address);
             return false;
         }
 
-        *value = machine->ram.memory[true_address];
-        return true;
+        *physical_address = virtual_address;
     }
 
-    if (address >= machine->ram.memory_size) {
-        raise_exception(machine, GP, address);
-        return false;
-    }
+    return true;
+}
 
-    *value = machine->ram.memory[address];
+bool read_byte(Machine *machine, const u16 address, u64 *value) {
+    u64 physical_address;
+    if (!resolve_address(machine, address, &physical_address)) return false;
+
+    *value = machine->ram.memory[physical_address];
     return true;
 }
 
 bool read_word(Machine *machine, const u64 address, u64 *value) {
-    const CPU *cpu = &machine->cpu;
-    if (cpu->sys.FR.V  == 1) {
-        u32 true_address;
-        bool success = translate(machine, address, &true_address);
-        if (!success) {
-            raise_exception(machine, PF, address);
-            return false;
-        }
+    u64 tmp;
 
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
+    if (!read_byte(machine, address, value)) return false;
+    if (!read_byte(machine, address + 1, &tmp)) return false;
 
-        *value = machine->ram.memory[true_address];
+    *value |= tmp << 8 & 0xFF00;
 
-        success = translate(machine, address + 1, &true_address);
-        if (!success) {
-            raise_exception(machine, PF, address);
-            return false;
-        }
-
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
-
-        *value |= machine->ram.memory[true_address];
-        return true;
-    }
-
-    if (address+1 >= machine->ram.memory_size) {
-        raise_exception(machine, GP, address);
-        return false;
-    }
-
-    *value = machine->ram.memory[address] | machine->ram.memory[address + 1] << 8;
     return true;
 }
 
 bool write_byte(Machine *machine, const u16 address, const u8 value) {
-    const CPU *cpu = &machine->cpu;
-    if (cpu->sys.FR.V  == 1) {
-        u32 true_address = 0;
-        const bool success = translate(machine, address, &true_address);
-        if (!success) {
-            raise_exception(machine, PF, address);
-            return false;
-        }
+    u64 physical_address;
 
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
+    if (!resolve_address(machine, address, &physical_address)) return false;
 
-        machine->ram.memory[true_address] = value;
-    } else {
-        if (address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
-
-        machine->ram.memory[address] = value;
-    }
+    machine->ram.memory[address] = value;
 
     return true;
 }
 
 bool write_word(Machine *machine, const u16 address, const u16 value) {
-    const CPU *cpu = &machine->cpu;
-    if (cpu->sys.FR.V == 1) {
-        u32 true_address = 0;
-        bool success = translate(machine, address, &true_address);
-        if (!success) {
-            raise_exception(machine, PF, address);
-            return false;
-        }
-
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
-
-        machine->ram.memory[true_address] = value & 0xFF;
-
-        success = translate(machine, true_address + 1, &true_address);
-        if (!success) {
-            raise_exception(machine, PF, address);
-            return false;
-        }
-
-        if (true_address >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
-
-        machine->ram.memory[true_address] = value >> 8 & 0xFF;
-    } else {
-        if (address+1 >= machine->ram.memory_size) {
-            raise_exception(machine, GP, address);
-            return false;
-        }
-
-        machine->ram.memory[address] = value & 0xFF;
-        machine->ram.memory[address + 1] = value >> 8 & 0xFF;
-    }
-
-    return true;
+    return write_byte(machine, address, value & 0xFF) &&
+        write_byte(machine, address + 1, value >> 8 & 0xFF);
 }
 
 bool fetch_byte(Machine *machine, u64 *value) {
