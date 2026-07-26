b755a085e1eebd7c9ff4173004c0f5b6257398f3
Bernardo <b.rego.oliveira@gmail.com>
2026-07-27 00:41:24 +0100

    compiler: test lexer

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -1,10 +1,32 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 
 #include "target/target.h"
 #include "type.h"
+#include "lexer/lexer.h"
 
 int main(void) {
+    SourceFile file = {0};
+
+    source_file_load(&file, "test.c");
+
+    Lexer lexer;
+
+    lexer_init(&lexer, &file);
+
+    PPToken tok;
+    LexerError error;
+    while (true) {
+        lexer_next(&lexer, &tok, &error);
+
+        if (tok.kind == PP_TOKEN_EOF) {
+            break;
+        }
+    }
+
+    source_file_destroy(&file);
+
     return EXIT_SUCCESS;
 }
