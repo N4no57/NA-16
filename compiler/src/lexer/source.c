9295bfc300bca82b6fea113534bfba9f87c99bcb
Bernardo <b.rego.oliveira@gmail.com>
2026-07-27 00:30:03 +0100

    compiler: added strdup so that path can be placed into SourceFile struct

--- compiler/src/lexer/source.c
+++ compiler/src/lexer/source.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 bool source_file_load(SourceFile *source, const char *path) {
     FILE *f = fopen(path, "r");
@@ -10,16 +11,20 @@ bool source_file_load(SourceFile *source, const char *path) {
 
     fseek(f, 0, SEEK_END);
     source->length = (size_t)ftell(f);
-    source->contents = malloc(source->length);
+    source->contents = malloc(source->length+1);
     fseek(f, 0, SEEK_SET);
     fread(source->contents, 1, source->length, f);
     fclose(f);
+
+    source->contents[source->length] = '\0';
+    source->path = strdup(path);
 
     return true;
 }
 
 void source_file_destroy(SourceFile *source) {
     free(source->contents);
+    free(source->path);
     source->contents = nullptr;
     source->path = nullptr;
     source->length = 0;
