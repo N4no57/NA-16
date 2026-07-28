39f198814c13bc975962ab59af65190d1387eea3
Bernardo <b.rego.oliveira@gmail.com>
2026-07-28 23:46:14 +0100

    compiler: macro list to have macros for preprocessor

---
+++ compiler/src/preprocessor/macro.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "macro.h"
+
+Macro *macro_table_find(const MacroTable *table, const char *name) {
+    for (size_t i = 0; i < table->count; i++) {
+        if (table->entries[i].name == nullptr) continue;
+
+        if (strcmp(table->entries[i].name, name) == 0) {
+            return &table->entries[i];
+        }
+    }
+
+    return nullptr;
+}
+
+bool macro_table_define(MacroTable *table, const Macro macro) {
+    if (macro_table_find(table, macro.name) == nullptr) {
+        return false;
+    }
+
+    if (table->count >= table->capacity) {
+        if (table->capacity == 0) table->capacity = 8;
+        else table->capacity *= 2;
+
+        Macro *tmp = realloc(table->entries, sizeof(Macro) * table->capacity);
+        if (tmp == nullptr) {
+            return false;
+        }
+
+        table->entries = tmp;
+    }
+
+    table->entries[table->count] = macro;
+    table->count++;
+
+    return true;
+}
+
+void macro_table_undef(const MacroTable *table, const char *name) {
+    Macro *macro = macro_table_find(table, name);
+
+    free(macro->name );
+    macro->name = nullptr;
+}
---
+++ compiler/src/preprocessor/macro.h
@@ -0,0 +1,31 @@
+#ifndef NA_16_MACRO_H
+#define NA_16_MACRO_H
+
+#include "../lexer/lexer.h"
+
+typedef struct Macro {
+    char *name;
+
+    char **parameters;
+    size_t parameter_count;
+    bool variadic;
+
+    PPToken *replacement;
+    size_t replacement_count;
+
+    SourceSpan definition_span;
+} Macro;
+
+typedef struct MacroTable {
+    Macro *entries;
+    size_t count;
+    size_t capacity;
+} MacroTable;
+
+Macro *macro_table_find(const MacroTable *table, const char *name);
+
+bool macro_table_define(MacroTable *table, Macro macro);
+
+void macro_table_undef(const MacroTable *table, const char *name);
+
+#endif //NA_16_MACRO_H
