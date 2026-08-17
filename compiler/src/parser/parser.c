0de7d7d99a558d81fc3dba46b25027a3a21a82ef
Bernardo <b.rego.oliveira@gmail.com>
2026-08-17 11:15:31 +0100

    compiler: rename parser in functions to p; what the fuck are declarators bro what is this bullshit?

--- compiler/src/parser/parser.c
+++ compiler/src/parser/parser.c
@@ -3,34 +3,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-void parser_init(Parser *parser, TokenStream *tokens) {
-    parser->tokens = tokens;
+void parser_init(Parser *p, TokenStream *tokens) {
+    p->tokens = tokens;
 
-    vector_init(&parser->errors, sizeof(ParserError));
+    vector_init(&p->errors, sizeof(ParserError));
 }
 
-static const CToken *parser_peek(const Parser *parser, size_t lookahead) {
-    return token_stream_peek(parser->tokens, lookahead);
+static const CToken *parser_peek(const Parser *p, size_t lookahead) {
+    return token_stream_peek(p->tokens, lookahead);
 }
 
-static Error parser_consume(const Parser *parser, CToken *token) {
-    return token_stream_consume(parser->tokens, token);
+static Error parser_consume(const Parser *p, CToken *token) {
+    return token_stream_consume(p->tokens, token);
 }
 
-static Error parser_match(const Parser *parser, const CTokenKind expected) {
-    return token_stream_match(parser->tokens, expected);
+static Error parser_match(const Parser *p, const CTokenKind expected) {
+    return token_stream_match(p->tokens, expected);
 }
 
-static Error parser_expected(Parser *parser, const CTokenKind expected, CToken *result) {
-    const CToken *token = parser_peek(parser, 0);
+static Error parser_expected(Parser *p, const CTokenKind expected, CToken *result) {
+    const CToken *token = parser_peek(p, 0);
 
     if (token == nullptr) {
         return ERROR_NULL_POINTER;
     }
 
     if (token->kind != expected) {
-        // parser->error.span = token->span;
-        // parser->error.message = "Unexpected token";
+        // p->error.span = token->span;
+        // p->error.message = "Unexpected token";
 
         return ERROR_NOT_FOUND;
     }
@@ -39,7 +39,7 @@ static Error parser_expected(Parser *parser, const CTokenKind expected, CToken *
         *result = *token;
     }
 
-    return token_stream_consume(parser->tokens, nullptr);
+    return token_stream_consume(p->tokens, nullptr);
 }
 
 static bool is_storage_class_specifier(const CTokenKind tok) {
@@ -116,15 +116,15 @@ static Precedence get_precedence(CTokenKind operator) {
     }
 }
 
-Expr *parse_expression(Parser *parser, float min_bp) {
-    const CToken *tok = parser_peek(parser, 0);
+Expr *parse_expression(Parser *p, float min_bp) {
+    const CToken *tok = parser_peek(p, 0);
     Expr *left = nullptr;
 
     if (tok->kind != C_TOKEN_LEFT_PAREN) {
-        parser_consume(parser, nullptr);
-        left = parse_expression(parser, 0);
+        parser_consume(p, nullptr);
+        left = parse_expression(p, 0);
 
-        tok = parser_peek(parser, 0);
+        tok = parser_peek(p, 0);
         if (tok->kind != C_TOKEN_RIGHT_PAREN) {
             return nullptr;
         }
@@ -134,7 +134,7 @@ Expr *parse_expression(Parser *parser, float min_bp) {
     left = malloc(sizeof(Expr));
 
     while (true) {
-        tok = parser_peek(parser, 0);
+        tok = parser_peek(p, 0);
 
         if (
             tok->kind == C_TOKEN_EOF ||
@@ -142,28 +142,28 @@ Expr *parse_expression(Parser *parser, float min_bp) {
             break;
         }
 
-        parser_consume(parser, nullptr);
+        parser_consume(p, nullptr);
 
         float r_bp = 0;
 
-        Expr *right = parse_expression(parser, r_bp);
+        Expr *right = parse_expression(p, r_bp);
     }
 
     return left;
 }
 
-Error parse_jump_statement(Parser *parser, Statement *result) {
+Error parse_jump_statement(Parser *p, Statement *result) {
     Error code;
     CToken return_token;
 
-    if ((code = parser_expected(parser, C_TOKEN_KW_RETURN, &return_token)) != ERROR_OK) {
+    if ((code = parser_expected(p, C_TOKEN_KW_RETURN, &return_token)) != ERROR_OK) {
         return code;
     }
 
     Expr *expression = nullptr;
 
-    if (parser_peek(parser, 0)->kind != C_TOKEN_SEMICOLON) {
-        expression = parse_expression(parser, 0);
+    if (parser_peek(p, 0)->kind != C_TOKEN_SEMICOLON) {
+        expression = parse_expression(p, 0);
 
         if (expression == nullptr) {
             return ERROR_NULL_POINTER;
@@ -172,7 +172,7 @@ Error parse_jump_statement(Parser *parser, Statement *result) {
 
     CToken semicolon;
 
-    if ((code = parser_expected(parser, C_TOKEN_SEMICOLON, &semicolon)) != ERROR_OK) {
+    if ((code = parser_expected(p, C_TOKEN_SEMICOLON, &semicolon)) != ERROR_OK) {
         expression_destroy(expression);
         return code;
     }
@@ -192,12 +192,12 @@ Error parse_jump_statement(Parser *parser, Statement *result) {
     return ERROR_OK;
 }
 
-Error parse_compound_statement(Parser *parser, Statement *result);
+Error parse_compound_statement(Parser *p, Statement *result);
 
-Error parse_statement(Parser *parser, Statement *result) {
-    const CToken *token = parser_peek(parser, 0);
+Error parse_statement(Parser *p, Statement *result) {
+    const CToken *token = parser_peek(p, 0);
 
-    if (token->kind == C_TOKEN_LEFT_BRACE) return parse_compound_statement(parser, result);
+    if (token->kind == C_TOKEN_LEFT_BRACE) return parse_compound_statement(p, result);
 
     if (token->kind == C_TOKEN_LEFT_PAREN ||
         token->kind == C_TOKEN_IDENTIFIER ||
@@ -206,7 +206,7 @@ Error parse_statement(Parser *parser, Statement *result) {
         token->kind == C_TOKEN_INTEGER_CONSTANT ||
         token->kind == C_TOKEN_STRING_LITERAL
     ) {
-        Expr *expression = parse_expression(parser, 0);
+        Expr *expression = parse_expression(p, 0);
         if (expression == nullptr) return ERROR_NULL_POINTER;
 
         result->kind = STATEMENT_EXPRESSION;
@@ -220,28 +220,28 @@ Error parse_statement(Parser *parser, Statement *result) {
         token->kind == C_TOKEN_KW_GOTO ||
         token->kind == C_TOKEN_KW_CONTINUE
     ) {
-        return parse_jump_statement(parser, result);
+        return parse_jump_statement(p, result);
     }
 
     return ERROR_OK;
 }
 
-Error parse_compound_statement(Parser *parser, Statement *result) {
+Error parse_compound_statement(Parser *p, Statement *result) {
     Error code;
     CToken left_brace;
 
-    if ((code = parser_expected(parser, C_TOKEN_LEFT_BRACE, &left_brace)) != ERROR_OK) {
+    if ((code = parser_expected(p, C_TOKEN_LEFT_BRACE, &left_brace)) != ERROR_OK) {
         return code;
     }
 
     vector_init(&result->data.compound_statement.items, sizeof(BlockItem));
 
-    while (parser_peek(parser, 0)->kind != C_TOKEN_RIGHT_BRACE &&
-           parser_peek(parser, 0)->kind != C_TOKEN_EOF) {
+    while (parser_peek(p, 0)->kind != C_TOKEN_RIGHT_BRACE &&
+           parser_peek(p, 0)->kind != C_TOKEN_EOF) {
         BlockItem item;
         item.kind = BLOCK_ITEM_STATEMENT;
 
-        if ((code = parse_statement(parser, &item.data.statement)) != ERROR_OK) {
+        if ((code = parse_statement(p, &item.data.statement)) != ERROR_OK) {
             vector_destroy(&result->data.compound_statement.items);
             return code;
         }
@@ -255,7 +255,7 @@ Error parse_compound_statement(Parser *parser, Statement *result) {
 
     CToken right_brace;
 
-    if ((code = parser_expected(parser, C_TOKEN_RIGHT_BRACE, &right_brace)) != ERROR_OK) {
+    if ((code = parser_expected(p, C_TOKEN_RIGHT_BRACE, &right_brace)) != ERROR_OK) {
         vector_destroy(&result->data.compound_statement.items);
         return code;
     }
@@ -268,13 +268,13 @@ Error parse_compound_statement(Parser *parser, Statement *result) {
     return ERROR_OK;
 }
 
-Error parse_function_definition(Parser *parser, FunctionDefinition *function) {
+Error parse_function_definition(Parser *p, FunctionDefinition *function) {
     Error code;
 
     {
         CToken function_name;
 
-        if ((code = parser_expected(parser, C_TOKEN_IDENTIFIER, &function_name)) != ERROR_OK) {
+        if ((code = parser_expected(p, C_TOKEN_IDENTIFIER, &function_name)) != ERROR_OK) {
             return code;
         }
 
@@ -285,14 +285,86 @@ Error parse_function_definition(Parser *parser, FunctionDefinition *function) {
 
     if (function->body == nullptr) return ERROR_ALLOCATION_FAILED;
 
-    if ((code = parse_compound_statement(parser, function->body)) != ERROR_OK) {
+    if ((code = parse_compound_statement(p, function->body)) != ERROR_OK) {
         return code;
     }
 
     return ERROR_OK;
 }
 
-Error parse_declaration_specifiers(Parser *parser, DeclarationSpecifiers *result) {
+Error parse_storage_class(Parser *p, DeclarationSpecifiers *spec) {
+    const CToken *tok = parser_peek(p, 0);
+
+    if (spec->storage_class != STORAGE_NONE) {
+        return ERROR_INTERNAL; // TODO error
+    }
+
+    if (tok->kind == C_TOKEN_KW_TYPEDEF) spec->storage_class = STORAGE_TYPEDEF;
+    else if (tok->kind == C_TOKEN_KW_EXTERN) spec->storage_class = STORAGE_EXTERN;
+    else if (tok->kind == C_TOKEN_KW_STATIC) spec->storage_class = STORAGE_STATIC;
+    else if (tok->kind == C_TOKEN_KW_AUTO) spec->storage_class = STORAGE_AUTO;
+    else if (tok->kind == C_TOKEN_KW_REGISTER) spec->storage_class = STORAGE_REGISTER;
+
+    return parser_consume(p, nullptr);
+}
+
+Error parse_type_qualifier(Parser *p, DeclarationSpecifiers *spec) {
+    const CToken *tok = parser_peek(p, 0);
+
+    if (tok->kind == C_TOKEN_KW_CONST) {
+        if (spec->type_qualifiers & CTYPE_QUALIFIER_CONST) {
+            return ERROR_INTERNAL; // TODO error
+        }
+
+        spec->type_qualifiers |= CTYPE_QUALIFIER_CONST;
+    } else if (tok->kind == C_TOKEN_KW_RESTRICT) {
+        if (spec->type_qualifiers & C_TOKEN_KW_RESTRICT) {
+            return ERROR_INTERNAL; // TODO error
+        }
+
+        spec->type_qualifiers |= C_TOKEN_KW_RESTRICT;
+    } else if (tok->kind == C_TOKEN_KW_VOLATILE) {
+        if (spec->type_qualifiers & C_TOKEN_KW_VOLATILE) {
+            return ERROR_INTERNAL; // TODO error
+        }
+
+        spec->type_qualifiers |= C_TOKEN_KW_VOLATILE;
+    }
+
+    return parser_consume(p, nullptr);
+}
+
+constexpr TypeSpecifier type_specifier_converter[C_TOKEN_COUNT] = {
+    [C_TOKEN_KW_VOID] = TYPE_SPEC_VOID,
+    [C_TOKEN_KW_CHAR] = TYPE_SPEC_CHAR,
+    [C_TOKEN_KW_SHORT] = TYPE_SPEC_SHORT,
+    [C_TOKEN_KW_INT] = TYPE_SPEC_INT,
+    [C_TOKEN_KW_LONG] = TYPE_SPEC_LONG,
+    [C_TOKEN_KW_FLOAT] = TYPE_SPEC_FLOAT,
+    [C_TOKEN_KW_DOUBLE] = TYPE_SPEC_DOUBLE,
+    [C_TOKEN_KW_SIGNED] = TYPE_SPEC_SIGNED,
+    [C_TOKEN_KW_UNSIGNED] = TYPE_SPEC_UNSIGNED,
+};
+
+Error parse_type_specifier(Parser *p, DeclarationSpecifiers *spec) {
+    const CToken *tok = parser_peek(p, 0);
+
+    const TypeSpecifier specifier = type_specifier_converter[tok->kind];
+
+    if (spec->type_specifiers & specifier &&
+        (spec->type_qualifiers & TYPE_SPEC_LONG && spec->long_count >= 2) != specifier
+    ) {
+        return ERROR_INTERNAL; // TODO error
+    }
+
+    spec->type_specifiers |= specifier;
+
+    if (spec->type_qualifiers & TYPE_SPEC_LONG) spec->long_count++;
+
+    return parser_consume(p, nullptr);
+}
+
+Error parse_declaration_specifiers(Parser *p, DeclarationSpecifiers *result) {
     DeclarationSpecifiers spec = {
         .storage_class = STORAGE_NONE,
         .type_qualifiers = 0,
@@ -301,33 +373,31 @@ Error parse_declaration_specifiers(Parser *parser, DeclarationSpecifiers *result
     };
 
     while (true) {
-        const CToken *tok = parser_peek(parser, 0);
+        const CToken *tok = parser_peek(p, 0);
+        Error code;
 
         if (is_storage_class_specifier(tok->kind)) {
             // parse storage class
-            if (spec.storage_class != STORAGE_NONE) {
-                return ERROR_INTERNAL; // TODO error
-            }
-
-            if (tok->kind == C_TOKEN_KW_TYPEDEF) spec.storage_class = STORAGE_TYPEDEF;
-            else if (tok->kind == C_TOKEN_KW_EXTERN) spec.storage_class = STORAGE_EXTERN;
-            else if (tok->kind == C_TOKEN_KW_STATIC) spec.storage_class = STORAGE_STATIC;
-            else if (tok->kind == C_TOKEN_KW_AUTO) spec.storage_class = STORAGE_AUTO;
-            else if (tok->kind == C_TOKEN_KW_REGISTER) spec.storage_class = STORAGE_REGISTER;
-        } else if (is_type_qualifier(tok->kind)) {
-            // parse type qualifier
-            if (tok->kind == C_TOKEN_KW_CONST) {
-                if () {
-                    return ERROR_INTERNAL; // TODO error
-                }
-
-                spec.type_qualifiers |= CTYPE_QUALIFIER_CONST;
-            }
-        } else if (is_type_specifier(tok->kind)) {
-            // parse type specifier
-        } else {
-            break;
+            code = parse_storage_class(p, &spec);
+            if (code != ERROR_OK) return code;
+            continue;
         }
+
+        if (is_type_qualifier(tok->kind)) {
+            // parse type qualifier
+            code = parse_type_qualifier(p, &spec);
+            if (code != ERROR_OK) return code;
+            continue;
+        }
+
+        if (is_type_specifier(tok->kind)) {
+            // parse type specifier
+            code = parse_type_specifier(p, &spec);
+            if (code != ERROR_OK) return code;
+            continue;
+        }
+
+        break;
     }
 
     // validate declaration specifiers
@@ -335,35 +405,168 @@ Error parse_declaration_specifiers(Parser *parser, DeclarationSpecifiers *result
     return ERROR_OK;
 }
 
-Error parse_external_declaration(Parser *parser, TranslationUnit *unit) {
+static void declarator_insert_inner(Declarator **root, Declarator *node) {
+    Declarator **slot = root;
+
+    while ((*slot)->kind != DECL_IDENTIFIER) {
+        switch ((*slot)->kind) {
+            case DECL_POINTER:
+                slot = &(*slot)->pointer.inner;
+                break;
+
+            case DECL_ARRAY:
+                slot = &(*slot)->array.inner;
+                break;
+
+            case DECL_FUNCTION:
+                slot = &(*slot)->function.inner;
+                break;
+
+            default:
+                // impossible
+                break;
+        }
+    }
+
+    node->pointer.inner = *slot; // conceptually node->inner
+    *slot = node;
+}
+
+Error parse_pointer_prefix(Parser *p, Vector *pointer_list) {
+    const CToken *tok = parser_peek(p, 0);
+    vector_init(pointer_list, sizeof(Declarator *));
+
+    while (tok->kind == C_TOKEN_ASTERISK) {
+        Declarator *declarator = malloc(sizeof(Declarator));
+        Error code = parser_consume(p, nullptr); // consume *
+        if (code != ERROR_OK) return code;
+
+        declarator->kind = DECL_POINTER;
+
+        {
+            DeclarationSpecifiers spec = {0};
+            while (is_type_qualifier(tok->kind)) {
+                code = parse_type_specifier(p, &spec);
+                if (code != ERROR_OK) return code;
+            }
+            declarator->pointer.qualifiers = spec.type_qualifiers;
+        }
+
+        code = vector_push(pointer_list, &declarator);
+        if (code != ERROR_OK) return code;
+        tok = parser_peek(p, 0);
+    }
+
+    return ERROR_OK;
+}
+
+Error parse_declarator_suffix(Parser *p, Declarator **result) {
+    const CToken *tok = parser_peek(p, 0);
+    Error code;
+
+    Declarator *decl = malloc(sizeof(Declarator));
+    if (*result != nullptr) return ERROR_ALLOCATION_FAILED;
+
+    if (tok->kind == C_TOKEN_LEFT_PAREN) {
+        // this is a function... thingy. BRING IN THE ASSAULT RIFLE.
+
+        code = parser_match(p, C_TOKEN_RIGHT_PAREN);
+        if (code != ERROR_OK) return code;
+    } else if (tok->kind == C_TOKEN_LEFT_BRACKET) {
+        // this is an array; GET EM!!!!
+        code = parser_consume(p, nullptr);
+        if (code != ERROR_OK) return code;
+
+        code = parser_match(p, C_TOKEN_RIGHT_BRACKET);
+        if (code != ERROR_OK) return code;
+    }
+
+    *result = decl;
+    return ERROR_OK;
+}
+
+Error parse_declarator(Parser *p, Declarator **result) {
+    Declarator *decl;
+    Vector pointer_list = {0};
+
+    Error code = parse_pointer_prefix(p, &pointer_list);
+    if (code != ERROR_OK) return code;
+
+    const CToken *tok = parser_peek(p, 0);
+
+    if (tok->kind == C_TOKEN_IDENTIFIER) {
+        decl = malloc(sizeof(Declarator));
+        decl->kind = DECL_IDENTIFIER;
+        memcpy(&decl->identifier, tok, sizeof(CToken));
+        if ((code = parser_consume(p, nullptr)) != ERROR_OK) return code;
+    } else if (tok->kind == C_TOKEN_LEFT_PAREN) {
+        parser_consume(p, nullptr);
+        code = parse_declarator(p, &decl);
+        if (code != ERROR_OK) return code;
+
+        if ((code = parser_match(p, C_TOKEN_RIGHT_PAREN)) != ERROR_OK) return code;
+    } else {
+        return ERROR_INTERNAL; // TODO error
+    }
+
+    tok = parser_peek(p, 0);
+
+    while (tok->kind == C_TOKEN_LEFT_PAREN || tok->kind == C_TOKEN_LEFT_BRACKET) {
+        Declarator *suffix;
+        code = parse_declarator_suffix(p, &suffix);
+        if (code != ERROR_OK) return code;
+
+        tok = parser_peek(p, 0);
+        declarator_insert_inner(&decl, suffix);
+    }
+
+    for (size_t i = pointer_list.length; i > 0; i--) {
+        declarator_insert_inner(
+            &decl,
+            ((Declarator **)pointer_list.data)[i - 1]
+        );
+    }
+
+    *result = decl;
+    return ERROR_OK;
+}
+
+Error parse_external_declaration(Parser *p, TranslationUnit *unit) {
     Error code;
     ExternalDeclaration external_declaration = {0};
 
     DeclarationSpecifiers specs;
-    code = parse_declaration_specifiers(parser, &specs);
+    code = parse_declaration_specifiers(p, &specs);
     if (code != ERROR_OK) return code;
 
-    Declarator declarator; // = parse_declarator(parser);
+    Declarator *declarator;
+    code = parse_declarator(p, &declarator);
+    if (code != ERROR_OK) return code;
 
-    if (declarator.kind == DECL_FUNCTION &&
-        parser_peek(parser, 0)->kind == C_TOKEN_LEFT_BRACE) {
+    if (declarator->kind == DECL_FUNCTION &&
+        parser_peek(p, 0)->kind == C_TOKEN_LEFT_BRACE) {
         external_declaration.kind = EXTERNAL_DECLARATION_FUNCTION_DEFINITION;
         external_declaration.data.function_definition.specifiers = specs;
-        external_declaration.data.function_definition.declarator = declarator;
+        external_declaration.data.function_definition.declarator = *declarator;
+        free(declarator);
 
-        code = parse_function_definition(parser, &external_declaration.data.function_definition);
+        code = parse_function_definition(p, &external_declaration.data.function_definition);
         if (code != ERROR_OK) return code;
     } else {
+        external_declaration.kind = EXTERNAL_DECLARATION_DECLARATION;
+        external_declaration.data.declaration.specifiers = specs;
 
+        // code = parse_declaration(p, &external_declaration.data.declaration);
+        if (code != ERROR_OK) return code;
     }
 
     return vector_push(unit, &external_declaration);
 }
 
-Error parse_translation_unit(Parser *parser, TranslationUnit *unit) {
+Error parse_translation_unit(Parser *p, TranslationUnit *unit) {
     Error code;
-    while (parser_peek(parser, 0)->kind != C_TOKEN_EOF) {
-        if ((code = parse_external_declaration(parser, unit))) return code;
+    while (parser_peek(p, 0)->kind != C_TOKEN_EOF) {
+        if ((code = parse_external_declaration(p, unit)) != ERROR_OK) return code;
     }
 
     return ERROR_OK;
